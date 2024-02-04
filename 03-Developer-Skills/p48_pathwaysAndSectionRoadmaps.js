f1a3c860a08083ef5b5d15765b5ed47fec66db2e
Congying Zhao <t3cozh00@students.oamk.fi>
2024-02-04 13:18:36 +0200

    p48-50 someSettings

---
+++ 03-Developer-Skills/p48_pathwaysAndSectionRoadmaps.js
@@ -0,0 +1,23 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+/*
+  Section: DEVELOPER SKILLS & EDITOR SETUP
+  p48_051_pathwayAndSectionRoadmap
+  p49_053_sectionIntro
+  P50_055_setting up prettier and vs code
+     ⭕️ configure VS Code editor
+     start with a tool called Prettier: an opinionated code formatter(it makes assumptions about how good code should look like)
+     ⭕️ configure snippets: cl -> console.log();
+
+
+*/
+
+const x = '23';
+if (x === 23) console.log(23);
+
+const calcAverage = birthYear => 2037 - birthYear;
+
+console.log();
+
+
