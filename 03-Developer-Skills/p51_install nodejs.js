50da36b9a1411e0ee515e3016a15a52e595ce702
Congying Zhao <t3cozh00@students.oamk.fi>
2024-02-06 09:17:55 +0200

    p52

--- 03-Developer-Skills/p51_install nodejs.js
+++ 03-Developer-Skills/p51_install nodejs.js
@@ -6,4 +6,13 @@
 
 /*
   p52_learn how to code
+      how to fail at learning how to code
+      how to succeed at learning how to code
+      well-defined goal
+      share your goals to make yourself accountable
+*/
+
+/*
+  p53_how to think like a developer: become a problem solver
+      
 */
