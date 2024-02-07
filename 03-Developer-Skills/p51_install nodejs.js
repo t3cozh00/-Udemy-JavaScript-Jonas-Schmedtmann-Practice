06a887f6d3e98c41946e78b32e4bdd39928a7fea
Congying Zhao <t3cozh00@students.oamk.fi>
2024-02-07 19:56:16 +0200

    debug

--- 03-Developer-Skills/p51_install nodejs.js
+++ 03-Developer-Skills/p51_install nodejs.js
@@ -4,6 +4,7 @@
   sudo npm install live-server -g
 */
 
+
 /*
   p52_learn how to code
       how to fail at learning how to code
@@ -12,7 +13,54 @@
       share your goals to make yourself accountable
 */
 
+
 /*
   p53_how to think like a developer: become a problem solver
-      
+      ask the right questions to get a clear picture of the problem
+      divide and conquer: break a big problem into a smaller sub-problem
+      don't be afraid to do as much research as you have to 
+      for bigger problem, write pseudo-code before writing the actual code     
+*/
+
+/*
+  p54_using Google, StackOverflow and MDN
+  
+  ⚠️  EXAMPLE
+  
+  PROBLEM 1:
+  // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+   PROBLEM 2:
+  // Function should now receive 2 arrays of temps
+*/
+
+
+/*
+  p55_debugging (fixing errors)
+      -software bug
+      -becoming aware that there is a bug
+      -isloating where exactly the bug is happening in code
+      -correct the bug
+      -preventing it from happening again(searching for the same bug in similar code)
+*/
+
+/*
+  p56_debugging with the CONSOLE and BREAKPOINTS
+      put the debug processing into practice
+      how to use debugger in Google chrome
+*/
+
+/*
+   p57_Coding Challenge #1 
+   
+   Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+   Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+   Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+   Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+   TEST DATA 1: [17, 21, 23]
+   TEST DATA 2: [12, 5, -5, 0, 4]
 */
