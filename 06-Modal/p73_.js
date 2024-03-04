85327ccdefce03f8a747cd989a1cb9494bda5e6c
Congying Zhao <t3cozh00@students.oamk.fi>
2024-03-04 19:17:40 +0200

    0304project #2practice

---
+++ 06-Modal/p73_.js
@@ -0,0 +1,18 @@
+/***p73_
+ SECTION_JAVASCRIPT IN THE BROWSER: DOM AND EVENTS FUNDAMENTALS
+ LECTURE_PROJECT #2 MODAL WINDOW**************************
+    --- way of selecting multiple elements with the same class: document.querySelectorAll('.show-modal')
+    --- with for loop, to do some running to each of them, like logging their text content: console.log(btnsOpenModal[i].textContent)
+ */
+
+/***p74_
+ SECTION_JAVASCRIPT IN THE BROWSER: DOM AND EVENTS FUNDAMENTALS
+ LECTURE_WORKING WITH CLASSES*****************************
+    --- recap: if you want to use the same function in multiple event listeners, then you need to specify that function as a seperate function, then pass that function as an argument to multiple 'addEventListener' methods
+    --- need to use adding and removing classed all the time in order to change the appearance of elements on our page because classes allow us to aggregate multiple CSS properties in just one. by adding and removing, we can activate and deactivate certain styles
+*/
+
+/***p75_
+ SECTION_JAVASCRIPT IN THE BROWSER: DOM AND EVENTS FUNDAMENTALS
+ LECTURE_HANDLING AN 'ESC' KEYPRESS EVENT*****************
+*/
